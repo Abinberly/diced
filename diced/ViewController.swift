765656c8cfd449dd857a017200d0eaee8e0a4df6
Abinberly <abinberly12@gmail>
2023-08-21 13:02:44 +0530

    Initial Commit

---
+++ diced/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  diced
+//
+//  Created by Zakkariya K.A on 21/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var dice1: UIImageView!
+    @IBOutlet weak var dice2: UIImageView!
+    override func viewDidLoad() {
+        @IBOutlet weak var score: UILabel!
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
